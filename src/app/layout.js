6adfc76e3947896e3b122b7211518b52130ede38
Prabhath Kulasooriya <prabhath.kulasooriya@gmail.com>
2026-05-21 14:10:57 +0530

    favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -17,6 +17,9 @@ const geistMono = Geist_Mono({
 export const metadata = {
   title: "NexLearn",
   description: "LMS for O/L'students",
+  icons: {
+    icon: "/favicon.svg",
+  },
 };
 
 export default function RootLayout({ children }) {
